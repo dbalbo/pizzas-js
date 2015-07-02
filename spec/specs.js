da6d684a359c945f1af4812cd553777ec03b574e
debbie <debbalbo@hotmail.com>
2015-07-02 09:05:27 -0700

    specs and code pass to establish cost of pizza bassed on users choices of toppings

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,9 @@ describe('Pizza', function() {
 		var cheesePizza = new Pizza()
 		expect(cheesePizza.toppings).to.eql([]);
 	});
+
+	it("will cost nothing until the user adds their own pizza with toppings", function() {
+		var testPizza = new Pizza()
+		expect(testPizza.cost).to.equal(0);
+	});
 });
