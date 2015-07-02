93670d56e9035ffc923c1164038df3b7b1209385
debbie <debbalbo@hotmail.com>
2015-07-02 09:52:48 -0700

    added code for pricing based on sizes and ability to add toppings to cheese pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,22 @@ function Pizza(toppings, cost, size) {
 	this.cost = 0
 	this.size = size
 }
+
+Pizza.prototype.cost = function() {
+		if (this.size === "Small") {
+		cost = 10;
+	}	else if (this.size === "Medium") {
+		cost = 15;
+	} 	else if (this.size === "Large") {
+		cost = 20;
+	}	else if (this.size === "Texas") {
+		cost = 50;
+	}
+	return cost + (this.toppings.length);
+	
+}
+
+Pizza.prototype.addTopping = function(type) {
+	this.toppings.push(type)
+	return this.toppings;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,15 @@ describe('Pizza', function() {
 		var testPizza = new Pizza()
 		expect(testPizza.cost).to.equal(0);
 	});
+
+	it("lets a user choose a size such as large, medium, or small", function() {
+		var testPizza = new Pizza('large')
+		expect(testPizza.size).to.eql('large');
+	});
+
+	it('will let user add a topping', function() {
+		var cheesePizza = new Pizza()
+		cheesePizza.addTopping("pepperoni")
+		expect(cheesePizza.toppings).to.eql(['pepperoni']);
+	});
 });
